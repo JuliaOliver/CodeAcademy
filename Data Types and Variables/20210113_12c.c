9a6849b9d2ed31e755fd3712c5e7c5853fab0045
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2021-01-14 14:13:42 +0200

    Create 20210113_12c.c

---
+++ Data Types and Variables/20210113_12c.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+    
+    printf("          #          \n");
+    printf("        # # #        \n");
+    printf("      # # # # #      \n");
+    printf("    # # # # # # #    \n");
+    printf("  # # # # # # # # #  \n");
+    printf("# # # # # # # # # # #\n");
+    printf("  # # # # # # # # #  \n");
+    printf("    # # # # # # #    \n");
+    printf("      # # # # #      \n");
+    printf("        # # #        \n");
+    printf("          #          \n");
+    
+    return 0;
+}
+
+/* Prints the shape. The shape should've been created 
+without using loops, only by using the printf() function */
