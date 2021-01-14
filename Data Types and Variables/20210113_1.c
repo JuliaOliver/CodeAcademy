a35e53e660c7f2013ae18f34981042ffc6998136
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2021-01-14 13:33:50 +0200

    Create 20210113_1.c

---
+++ Data Types and Variables/20210113_1.c
@@ -0,0 +1,15 @@
+/* Sum, char (255 + 10). */
+
+#include <stdio.h>
+
+int main(void){
+    char a = 255;
+    char b = 10;
+    char res = a + b;
+
+    printf("%d\n", res);
+    printf("%c", res);
+
+    return 0;
+}
+/* Prints 9 */
