c20222290162aac69b183aad91730d92cf073dc7
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2021-01-18 23:38:16 +0200

    Create 20210118_4_2.c

---
+++ Operators in C/20210118_4_2.c
@@ -0,0 +1,22 @@
+/* In what order will the operations be performed?
+true = x && y ^ x << 1;
+What will be printed on the screen? */
+
+#include <stdio.h>
+
+int main(void){
+
+    int x = 10;
+    int y = 10;
+    int true;
+
+    true = x && y ^ x << 1;
+
+    printf("True = %d", true);
+
+    return 0;
+}
+
+/* Prints True = 1 */
+/* The order of evaluation of all operations is from left to right and all have 
+the same priority, so the operations are performed one by one from left to right. */
