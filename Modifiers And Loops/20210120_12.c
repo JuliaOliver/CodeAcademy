0e98265b1a5dde9a5d22e2c5bec98aa3333b831e
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2021-01-22 10:49:17 +0200

    Create 20210120_12.c

---
+++ Modifiers And Loops/20210120_12.c
@@ -0,0 +1,41 @@
+/* Nested switch constructions */
+
+#include <stdio.h>
+
+int main()
+{
+    char szBuf[10] = "za";
+    printf("Codes %s -> ", szBuf);
+    switch (szBuf[0])
+    {
+    case 'x':
+        printf("No\n");
+        break;
+    case 'y':
+        printf("No\n");
+        break;
+    case 'z':
+        switch (szBuf[1])
+        {
+        case 'a':
+            printf(" Yes, ");
+            break;
+        case 'x':
+            printf(" No, ");
+            break;
+        default: 
+            break;
+        }
+        printf("Maybe\n");
+        break;
+    case 'a':
+        printf("Yes\n");
+        break;
+    default:
+        printf("Unknown\n");
+        break;
+    }
+    return 0;
+}
+
+/* Prints Codes za ->  Yes, Maybe */
