058dd1e1e97a80f72b70acf47a20fabaed749a4a
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2021-02-23 22:03:54 +0200

    Update 20210222_12.c

--- Unions, Bit Fields, Enumerator/20210222_12.c
+++ Unions, Bit Fields, Enumerator/20210222_12.c
@@ -11,7 +11,6 @@ types and print the size of the structure again. Rearrange them as a sequence, t
 char, char, short int, short int. How does the size of the structure change?
 Try the same with the #pragma pack preprocessor (1). */
 
-
 #include <stdio.h>
 
 struct tagStruct{
@@ -19,11 +18,23 @@ struct tagStruct{
     char charField: 6;
 };
 
+struct tagStruct2{
+    char charField2: 6;
+    char charField3: 6;
+    unsigned short int intField2: 3;
+    unsigned short int intField3: 3;
+};
+
 int main(void){
     struct tagStruct variable = {7, 'c'};
 
     printf("Size of sctuct: %d", sizeof(variable));
     printf("\nInteger: %u\nChar: %c", variable.intField, variable.charField);
+
+    struct tagStruct2 variable2 = {'c', 'c', 7, 7};
+
+    printf("\n\nSize of sctuct: %d", sizeof(variable2));
+    printf("\nChar1: %c\nChar2: %c\nInteger1: %u\nInteger2: %u", variable2.charField2, variable2.charField3, variable2.intField2, variable2.intField3);
 
     return 0;
 }
