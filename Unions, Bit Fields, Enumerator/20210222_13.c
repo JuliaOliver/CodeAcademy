f43f84aa6e7b759b028d1dcf8d492cedf0ae5e99
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2021-02-23 12:53:14 +0200

    Create 20210222_13.c

---
+++ Unions, Bit Fields, Enumerator/20210222_13.c
@@ -0,0 +1,7 @@
+/* Merge with members unsigned short and char. Limit the bits used to 6. 
+Initialize and display on the console. */
+
+
+/* Имах проблеми със битовите полета, когато сложа тип char, потърсих информация и
+излезе, че в битовите полета в С мога да използвам смао типове int, signed int или unsigned int.
+Не мога да намеря начин да реша задачата като имам char в битово поле */
