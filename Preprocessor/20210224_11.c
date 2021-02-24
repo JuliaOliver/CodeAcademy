20d97d530c9be19fba69cdbf4c0179b166a28671
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2021-02-25 00:49:56 +0200

    Create 20210224_11.c

---
+++ Preprocessor/20210224_11.c
@@ -0,0 +1,11 @@
+/* If we have a defined macro
+#define М
+which of the following definitions will fail:
+a) #if M
+b) #ifdef M
+c) #ifndef M
+d) #if defined (M)
+e) #if! defined (M) */
+
+/* ANSWER */
+/* c) #ifndef M and e) #if! defined (M) will fail */
