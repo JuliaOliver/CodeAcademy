379c7dd87528efae58491a19214a06c678f6cc40
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2021-02-25 00:47:28 +0200

    Create 20210224_7.c

---
+++ Preprocessor/20210224_7.c
@@ -0,0 +1,14 @@
+/* Write a header file with function declarations and put guards. 
+This reduces the time to compile large projects. */
+
+#ifndef _MYHEADER_H_
+#define _MYHEADER_H_()("My Header!")
+#include <stdio.h>
+
+int main(void){
+    printf("%s", _MYHEADER_H_());
+
+    #endif
+
+    return 0;
+}
