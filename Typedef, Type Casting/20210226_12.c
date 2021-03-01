6c0823c2b1a636e3b1018398a29f9eb81e6ed3ad
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2021-03-01 11:57:09 +0200

    Create 20210226_12.c

---
+++ Typedef, Type Casting/20210226_12.c
@@ -0,0 +1,66 @@
+/* Introduce the employees in a company in a structure with members: 
+serial number, name, surname, surname, position, length of service in years, 
+salary in BGN, index to the structure, describing its boss. Write a program that 
+introduces 10 employees, which are stored in an array of the described structures. 
+The entry of employees can be done in two passes, the first entry of all data 
+without the directory to the boss and the second pass, indicating to each employee 
+who is his boss. Dynamic memory allocation and typedef must be used in the solution. */
+
+#include <stdio.h>
+#define MAX 2
+#include <stdlib.h>
+
+typedef struct{
+    int number;
+    char first_name[64];
+    char middle_name[64];
+    char last_name[64];
+    char position[64];
+    float working_time;
+    float salary;
+    struct member *boss;
+}member;
+
+void fillArray(member *array, member *boss){
+    int i = 0;
+    for(i=0; i < MAX; i++){
+        printf("Member[%d]: \n", i + 1);
+        printf("Enter Number: ");
+        scanf("%d", &(array + i)->number);
+        printf("Enter first name, middle name and last name respectively in format A B C \n");
+        scanf("%s %s %s", &(array+i)->first_name, &(array+i)->middle_name, &(array+i)->last_name);
+        printf("Enter position working time and salary in format P W S .\n");
+        scanf("%s %f %f", &(array+i)->position, &(array+i)->working_time, &(array+i)->salary);
+    }
+    for(i=0; i < MAX; i++){
+        printf("Enter names for boss for employer[%d]: \n", i+1);
+        printf("Enter first name, middle name and last name respectively in format A B C \n");
+        scanf("%s %s %s", &(boss+i)->first_name, &(boss+i)->middle_name, &(boss+i)->last_name);     
+    }
+}
+
+void printArray(member *array, member *boss){
+    int i=0;
+    for(i=0; i < MAX; i++){
+        printf("\n");
+        printf("Member[%d]: Number: %d \n", i+1, (array+i)->number);
+        printf("First name: %s , middle name: %s , and last name: %s .\n", (array+i)->first_name, (array+i)->middle_name, (array+i)->last_name);
+        printf("Position: %s ,working time: %.2f years,  salary: %.2f \n", (array+i)->position, (array+i)->working_time, (array+i)->salary);
+        printf("His/Her boss: %s %s %s .\n", (boss+i)->first_name, (boss+i)->middle_name, (boss+i)->last_name);
+    }
+}
+
+int main(void){
+    member *array;
+    array = (member*)malloc(MAX * sizeof(member));
+    member *boss;
+    boss = (member*)malloc(MAX * sizeof(boss));
+
+    fillArray(array, boss);
+    printf("\n");
+    printArray(array, boss);
+    free(array);
+    free(boss);
+
+    return 0;
+}
