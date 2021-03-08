fb5a72fa8d7c85fd835b5b6b7b4519c313635c39
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2021-03-08 23:19:18 +0200

    Create 20210308_3.c

---
+++ Input&Output (File Handling)/20210308_3.c
@@ -0,0 +1,23 @@
+/* Create a test1.txt file in a directory of your choice. Put some data in it in 
+Latin - name, greeting, number. Open to read. Use fgetc (), which takes only 
+one char from the file. Make a while loop to write all the characters, check 
+for the end of the EOF file. Check that the file pointer is not NULL, if it is NULL, type an error. */
+
+#include <stdio.h>
+
+int main(){
+    FILE *fp;
+    int c;
+    fp = fopen("testFile.txt","r");
+    if(fp==NULL){
+        perror ("Error in opening file");
+        return (-1);
+    }
+    while ((c=fgetc(fp)) != EOF)
+        printf("%c", c);
+
+    fclose(fp);
+    fp=NULL;
+    
+    return 0;
+}
