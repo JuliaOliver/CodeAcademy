7cd2e56620fd032366699d287986110288294e19
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2021-03-20 12:11:11 +0200

    Create prototypes.h

---
+++ Personal Task/prototypes.h
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int lineSum(int *matrixRow, int col);
+short compareArrayLines(int *arr1, int *arr2, int col);
+void swap(int *xp, int *yp);
+void calculateAndSaveResult(int **matrix, int row, int col);
