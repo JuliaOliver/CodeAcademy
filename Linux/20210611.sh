380ed91d9ed8a318bee92b177d7a2cb11c6bb7fc
JuliaOliver <74153919+JuliaOliver@users.noreply.github.com>
2021-06-15 23:30:29 +0300

    Create 20210611.sh

---
+++ Linux/20210611.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+COUNT=11
+
+for i in {1..11}
+
+do
+
+	if [[ $i == $COUNT ]]
+  
+	then echo $(((RANDOM%32)+1)) $(((RANDOM%32)+1))
+  
+	else
+		echo $(((RANDOM%32)+1)) $(((RANDOM%32)+1)) $(((RANDOM%32)+1))
+  fi
+
+done
